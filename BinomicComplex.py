1a188eab98eb2c806bd45101f2db3431442a93c0
jcastropizzo <jcastropizzo@DESKTOP-K9J8T8S>
2019-07-04 01:22:34 -0300

    first commit

---
+++ BinomicComplex.py
@@ -0,0 +1,18 @@
+import math
+
+class BinomicComplex(object):
+
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def print(self):
+        return '', self.real, '+i', self.imaginary
+
+    @property
+    def arg(self):
+        return math.atan(self.imaginary/self.real)
+
+    @property
+    def module(self):
+        return math.sqrt((self.real*self.real) + (self.imaginary*self.imaginary))
---
+++ PolarComplex.py
@@ -0,0 +1,19 @@
+import math
+
+class PolarComplex(object):
+
+    def __init__(self, module, arg):
+        self.module = module
+        self.arg = arg
+
+    def print(self):
+        return '' + self.module + 'arg(' + self.arg + ')'
+
+    @property
+    def real(self):
+        return math.cos(self.arg) * self.module
+
+    @property
+    def imaginary(self):
+        return math.sin(self.arg) * self.module
+
